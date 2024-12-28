c6ed0e2dcff4c5794aea2383f767882995d88a88
Dewan Mukto <dewanmukto@gmail.com>
2024-12-29 02:13:38 +0600

    Updated the menu buttons and added character

--- main.py
+++ main.py
@@ -1,52 +1,19 @@
 '''Import statements for dependencies'''
+# Essential OpenGL modules
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+# Built-in Python modules
 from time import sleep
-from algorithms import drawLine, drawCircle, drawFilledRectangle, drawText
-''' Highlights of the syntax:
-drawLine(x1, y1, x2, y2, color), drawCircle(xi, yi, r, color), drawFilledRectangle(x1, y1, x2, y2, color), \
-    drawText(text, x, y, color, scale)
-'''
+# Project-specific custom modules
+from algorithms import drawLine, drawCircle, drawFilledCircle, drawFilledRectangle, drawText
+from userinferface import Button
+from character import drawCharacter
 
 '''Global variables/constants'''
 W_Width, W_Height = 450,750
 buttons = []
 hovered_button_index, pressed_button_index = -1, -1
-
-# UI component - button
-class Button:
-    def __init__(self, left, bottom, width, height, label, label_size=0.55, label_offset=0):
-        self.left = left
-        self.bottom = bottom
-        self.width = width
-        self.height = height
-        self.label = label
-        self.label_size = label_size
-        self.label_offset = label_offset
-        self.is_hovered = False
-        self.is_pressed = False
-
-    def draw(self):
-        # Calculate colors based on state
-        primary_color = (0.0, 0.5, 0.0) if self.is_hovered else (0.0, 1.0, 0.0)
-        accent_color = (0.0, 0.2, 0.0) if self.is_hovered else (0.0, 0.5, 0.0)
-        text_color = (1.0, 1.0, 1.0) if self.is_pressed else (0.0, 0.0, 0.0)
-        
-        # Adjust height when pressed
-        bottom = self.bottom - 10 if self.is_pressed else self.bottom
-        top = self.bottom + self.height - 10 if self.is_pressed else self.bottom + self.height
-
-        # Draw button
-        drawFilledRectangle(self.left, bottom, self.left + self.width, top, primary_color)  # Main button
-        drawFilledRectangle(self.left, bottom - 10, self.left + self.width, bottom, accent_color)  # Accent
-
-        # Calculate text position for centering
-        text_x = self.left + self.width // 2 - (len(self.label) * self.label_size * 6) // 2 + self.label_offset
-        text_y = 10 + bottom + self.height // 2 - self.label_size * 10 // 2
-        drawText(self.label, text_x, text_y, text_color, self.label_size)
-
- 
 
 def setup_buttons():
     global buttons
@@ -58,14 +25,14 @@ def setup_buttons():
     play_button_x = (W_Width - 180) // 2
     buttons.append(Button(play_button_x, y_position, 180, 100, "Play", label_offset=-55))
 
-
     # Left button
     left_button_x = play_button_x - 100
-    buttons.append(Button(left_button_x, y_position, 80, 100, "<", label_offset=-25))
+    buttons.append(Button(left_button_x, y_position, 80, 100, "?", label_offset=-20))
 
     # Right button
     right_button_x = play_button_x + 200
-    buttons.append(Button(right_button_x, y_position, 80, 100, ">", label_offset=-20))
+    buttons.append(Button(right_button_x, y_position, 80, 100, "$", label_offset=-20))
+
 
 def iterate():
     glViewport(0, 0, W_Width, W_Height)
@@ -77,18 +44,21 @@ def iterate():
 
 def showScreen():
     global buttons, W_Width, W_Height
+    glClearColor(0.08, 0.08, 0.08, 0.0)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     iterate()
 
     drawText("ZenG", W_Width//2 -150, W_Height//2 +250, (0.1,1.0,0.1), 1.00) # Title
     drawText("423 Group12", W_Width//2 -70, W_Height//2 +280, (0.5,0.5,0.5), 0.15) # Subtitle
+
+    drawCharacter(W_Width//2,W_Height//2)
    
     # Rendering the buttons
     for button in buttons:
         button.draw()
     
-    glutSwapBuffers() # Default function for GLUT
+    glutSwapBuffers()
 
 def passiveMouseListener(x, y):
     global buttons, hovered_button_index
